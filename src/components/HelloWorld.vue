dd21a1f98eb7de13fd9c9b0c19e494b8c3081a31
Brent Buchholz <brentbuchholz@gmail.com>
2023-04-05 11:01:10 -0500

    Flesh out rough ideas for CapMart and CupMart

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,36 +2,33 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      CapMart is a one-stop shop for all your cap and hat needs. We carry a wide variety of styles, from baseball caps to fedoras, and we're always adding new items to our collection. Whether you're looking for a new hat for work or a special occasion, we've got you covered.
+    </p><p>
+But CapMart is more than just a hat store. We're also excited to announce the launch of our new cup division! We carry a wide variety of cups, from coffee mugs to water bottles, and we're always adding new items to our collection. Whether you're looking for a new cup for work or a special occasion, we've got you covered.
+</p><p>
+So why shop at CapMart? Here are a few reasons:
+</p><p>
+  <ul>
+<li> We have a wide variety of hats and cups to choose from.</li>
+<li> We offer competitive prices.</li>
+<li> We offer fast and free shipping on orders over $50.</li>
+<li> We offer a satisfaction guarantee on all of our products.</li>
+</ul>
+</p><p>
+So what are you waiting for? Shop at CapMart today!
+</p><p>
+Here are some additional details about our new cup division:
+</p><p>
+  <ul>
+<li> We carry a wide variety of cup styles, from coffee mugs to water bottles.</li>
+<li> We offer a variety of cup materials, including plastic, glass, and metal.</li>
+<li> We offer a variety of cup sizes, from small to large.</li>
+<li> We offer a variety of cup colors and patterns.</li>
+<li> We offer a variety of cup prices, from affordable to high-end.</li>
+</ul>
+</p><p>
+We are confident that you will find the perfect cup for your needs at CapMart.
     </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress" target="_blank" rel="noopener">e2e-cypress</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
